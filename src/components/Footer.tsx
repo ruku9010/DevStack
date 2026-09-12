c581ddf6ae21358a82290c07ea3f45a51fe80f1b
ruku901 <rukunujjamans@gmail.com>
2026-09-12 14:57:07 +0600

    Footer completed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,43 +3,49 @@ import footer from "/src/assets/logo-text.png";
 const Footer = () => {
   return (
     <>
+      <div className="divider"></div>
+      <div className="grid grid-cols-1 w-[95%] mx-auto md:grid-cols-4 gap-8 textDesign p-10">
         <div>
-          <div>
-            <img src={footer} alt="Footer" />
-            <p>
-              Curated tools, technologies, and resources for developers building
-              modern software.
-            </p>
-            <div>
-              <a href="https://github.com/ruku9010">GitHub</a>
-            </div>
-            <div>
-              <a href="https://x.com/rukunujjamans">Twitter</a>
-            </div>
-            <div>
-              <a href="https://www.linkedin.com/in/ruku-sarker/">LinkedIn</a>
-            </div>
+          <img src={footer} alt="Footer" />
+          <p className="textDesign text-sm leading-6 mt-2 line-clamp-2">
+            Curated tools, technologies, and resources for developers building
+            modern software.
+          </p>
+          <div className="flex items-center gap-4 mt-4 font-semibold">
+            <a href="https://github.com/ruku9010">GitHub</a>
+
+            <a href="https://x.com/rukunujjamans">Twitter</a>
+
+            <a href="https://www.linkedin.com/in/ruku-sarker/">LinkedIn</a>
           </div>
-          <div>
-            <h3>PRODUCT</h3>
+        </div>
+        <div>
+          <h3 className="font-semibold text-black">PRODUCT</h3>
+          <div className="flex flex-col gap-2 mt-4">
             <a href="">Home</a>
             <a href="">Technologies</a>
             <a href="">Projects</a>
           </div>
-          <div>
-            <h3>COMPANY</h3>
+        </div>
+        <div>
+          <h3 className="font-semibold text-black">COMPANY</h3>
+          <div className="flex flex-col gap-2 mt-4">
             <a href="">About</a>
             <a href="">Contact</a>
             <a href="">Career</a>
           </div>
-          <div>
-            <h3>LEGAL</h3>
+        </div>
+        <div>
+          <h3 className="font-semibold text-black">LEGAL</h3>
+          <div className="flex flex-col gap-2 mt-4">
             <a href="">Privacy Policy</a>
             <a href="">Terms of Service</a>
           </div>
         </div>
+      </div>
+      <div className="flex flex-col w-[95%] mx-auto md:flex-row items-center justify-between mt-8 border-t border-gray-200 pt-4 textDesign p-10">
         <span>© 2023 DevStack. All rights reserved.</span>
-        <div>
+        <div className="flex items-center gap-4 mt-2 md:mt-0">
           <a href="">Privacy</a>
           <a href="">Terms</a>
         </div>
